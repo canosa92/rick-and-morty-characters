4befe6c9b7d262377a362e928b7fdd7b8acc54b7
Adrian <adrian>
2023-11-23 21:37:08 +0100

    corregido

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 //creamos las constantes con los elementos del Html que vamos a necesitar
-
-
-const prePage = document.getElementById('pre-page');
+let listaPersonajes = document.getElementById('character-list')
+const prePage = document.getElementById('prev-page');
 const nextPage = document.getElementById('next-page');
-const page = 1
+ let paginaInicial = 'https://rickandmortyapi.com/api/character/?page=1'
+let next
+let prev
 //creamos la función para mostrar el contenido de cada personaje en la página principal
-let url = `https://rickandmortyapi.com/api/character/?page=${page}`
 
-fetch(`${url}`)
-.then((response) => {
+const personajes = (page) =>{
+  fetch(page)
+  .then((response) => {
     if (!response.ok) {
         throw new Error('En estos momento no funcion');
       }
@@ -16,16 +17,32 @@ fetch(`${url}`)
     })
   .then((data) => {
     console.log(data)
-    const listaPersonajes = document.getElementById('character-list')
+    next= data.info.next
+    prev= data.info.prev
+    listaPersonajes.innerHTML=''
 
-    data.results.forEach((persona)=>{
+    data.results.forEach((personaje)=>{
         let templatePersoanjes =`
         <li>
-            <img src=${persona.image}>
-            <h4>Nombre: ${persona.name}</h4>
-            <h4>Especie: ${persona.species}</h4>
+        <div class="marco">
+            <img src=${personaje.image}>
+            <h4>Nombre:${personaje.name}</h4>
+            <h4>Especie: ${personaje.species}</h4>
+          
+        </div>
     </li>`   
     listaPersonajes.innerHTML +=templatePersoanjes
     })
     })
-  
+}
+
+nextPage.addEventListener('click',function(){
+personajes(next);
+}
+);
+prePage.addEventListener('click',function(){
+personajes(prev);
+}
+);
+
+personajes(paginaInicial)
